926d3e43aded6833f0b3513ede8434d93a3b87b9
Yaxuan Li <yaxuanl@andrew.cmu.edu>
2021-04-02 00:40:08 +0800

    change the html into changable version

--- homework_6/scripts.js
+++ homework_6/scripts.js
@@ -1,49 +1,5 @@
 let carts = document.querySelectorAll('.add-cart');
 
-let products = [
-    {
-        name: "Original",
-        price: "$3.00",
-        image: "images/Original.jpg",
-        alt: "Original flavor cinnamon rolls picture",
-    
-    },
-    {
-        name: "Blackberry",
-        price: "$4.00",
-        image: "images/Blackberry-Cinnamon-Rolls.jpg",
-        alt: "Blackberry Cinnamon Rolls picture",
-    
-    },
-    {
-        name: "Walnut",
-        price: "$3.50",
-        image: "images/Walnut-Cinnamon-Rolls.jpg",
-        alt: "Walnut Cinnamon Rolls picture",
-    
-    },
-    {
-        name: "Original (Gluten-free)",
-        price: "$3.50",
-        image: "images/Original-gluten-free.jpg",
-        alt: "Original gluten free cinnamon rolls picture",
-    
-    },
-    {
-        name: "Pumpkin Spice",
-        price: "$4.00",
-        image: "images/pumpkin-cinnamon-rolls.jpg",
-        alt: "pumpkin cinnamon rolls picture",
-    
-    },
-    {
-        name: "Caramel Pecan",
-        price: "$4.00",
-        image: "images/Caramel-Pecan-Cinnanmon-Rolls.jpg",
-        alt: "Caramel Pecan Cinnanmon Rolls picture",
-    
-    }
-]
 
 for(let i = 0; i < carts.length; i++){
     carts[i].addEventListener('click',()=>{
@@ -79,22 +35,8 @@ function updateNumbers(){
     viewNumbers();
 }
 
-// var items = [];
-
-// function addItem(){
-//     items.push("hahah");
-//     console.log(items);
-	
-// }
-
-// function showlLength(){
-//     items.length;
-//     console.log(items);
-	
-// }
-
 function Size() {  
-    var mylist1 = document.getElementById("myList1");
+    // var mylist1 = document.getElementById("myList1");
 
     // document.getElementById("favourite").value = mylist.options[mylist.selectedIndex].text;  
     } 
@@ -104,3 +46,85 @@ function Glazing() {
     // document.getElementById("favourite").value = mylist.options[mylist.selectedIndex].text;  
     } 
 
+
+/*** Object Constructors ***/
+function Original() {
+    this.name = "Original";
+    this.price = "$3.00";
+    this.image = "images/Original.jpg";
+    this.alt = "Original flavor cinnamon rolls picture";
+    document.getElementById("cinnanmon-img").setAttribute("src", this.image);
+    console.log("ADSFds");
+    document.getElementById("cinnanmon-img").setAttribute("alt", this.alt);
+  }
+
+function Blackberry() {
+    this.name = "Blackberry";
+    this.price = "$4.00";
+    this.image = "images/Blackberry-Cinnamon-Rolls.jpg";
+    this.alt = "Blackberry Cinnamon Rolls picture";
+  }
+
+function Walnut() {
+    this.name = "Walnut";
+    this.price = "$3.50";
+    this.image = "images/Walnut-Cinnamon-Rolls.jpg";
+    this.alt = "Walnut Cinnamon Rolls picture";
+  }
+
+function Original_free() {
+    this.name = "Original (Gluten-free)";
+    this.price = "$3.50";
+    this.image = "images/Original-gluten-free.jpg";
+    this.alt = "Original gluten free cinnamon rolls picture";
+  }
+
+function Pumpkin_Spice() {
+    this.name = "Pumpkin Spice";
+    this.price = "$4.00";
+    this.image = "images/pumpkin-cinnamon-rolls.jpg";
+    this.alt = "pumpkin cinnamon rolls picture";
+  }
+
+function Caramel_Pecan() {
+    this.name = "Caramel Pecan";
+    this.price = "$4.00";
+    this.image = "images/Caramel-Pecan-Cinnanmon-Rolls.jpg";
+    this.alt = "Caramel Pecan Cinnanmon Rolls picture";
+  }
+  
+  /*** Global Variables ***/
+//   var cinnanmons = [new Original(), new Blackberry(), new Walnut(), new Original_free(), Pumpkin_Spice(), Caramel_Pecan()];
+  
+  /*** Functions ***/
+  // generates either a Cat, Dog, or Bird with a random name and random age
+  function generateCinnanmon(number) {
+
+    var cinnanmonType = cinnanmons[number];
+  
+    if (cinnanmonType instanceof Cat) 
+    {
+      return new Cat(generateRandomName(), generateRandomAge());
+    } 
+    else if (cinnanmonType instanceof Dog) 
+    {
+      return new Dog(generateRandomName(), generateRandomAge());
+    } 
+    else if (cinnanmonType instanceof Bird) 
+    {
+      return new Bird(generateRandomName(), generateRandomAge());
+    }
+  }
+  
+  /*** Document Load ****/
+  function onLoad(number) {
+    // generate a random animal when the document opens
+    var cinnanmon = generateCinnanmon(number);
+    console.log(cinnanmon)
+    // update the page based on the animal properties
+    document.getElementById("cinnanmon_name").textContent = cinnanmon.name + "  " + animal.age + "years old";
+    document.getElementById("animal-properties").textContent = cinnanmon.name + "  " + animal.age + "years old";
+    document.getElementById("animal-properties").textContent = cinnanmon.name + "  " + animal.age + "years old";
+    document.getElementById("changable_picture").setAttribute("src", cinnanmon.image)
+  
+  };
